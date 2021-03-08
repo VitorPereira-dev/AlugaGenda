5f7beb55f9c763350a07f372fca6e3c57d0916d5
VitorPereira-dev <pereira33vitor@gmail.com>
2021-03-08 17:02:16 -0300

    :package:

--- pags/cadastro.php
+++ pags/cadastro.php
@@ -1,6 +1,24 @@
+<?php
+
+$cliente = (isset($_GET['clientefj'])==TRUE) ? $_GET['clientefj']:'';
+
+if($cliente == 1){
+    echo'<style type="text/css">
+            #cnpj{
+                display: none;
+            }
+        </style>';
+}
+else{
+    echo'<style type="text/css">
+            #cpf{
+                display: none;
+            }
+        </style>';
+}
+?>
 <!DOCTYPE html>
 <html>
-    
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -10,13 +28,12 @@
     <link rel="stylesheet" href="../css/bulma.min.css" />
     <link rel="stylesheet" type="text/css" href="../css/login.css">
 </head>
-
 <body>
     <section class="hero is-success is-fullheight">
         <div class="hero-body">
             <div class="container has-text-centered">
                 <div class="column is-4 is-offset-4">
-                    <h3 class="title has-text-grey">Sistema de Cadastro AlugaGenda</h3>
+                    <h3 class="title has-text-grey">Cadastro AlugaGenda</h3>
                     <!-- <div class="notification is-success">
                       <p>Cadastro efetuado!</p>
                       <p>Faça login informando o seu usuário e senha <a href="login.php">aqui</a></p>
@@ -24,26 +41,37 @@
                     <!-- <div class="notification is-info">
                         <p>O usuário escolhido já existe. Informe outro e tente novamente.</p>
                     </div> -->
-                    <div class="box">
-                        <form action="cadastrar.php" method="POST">
-                            <div class="field">
-                                <div class="control">
-                                        <h1 class="title has-text-grey" aling="Center">Escolha entre Física ou Jurídica:</h2>
-                                    <select name="fj" class="input is-large">
-                                        <option value=""></option>
-                                        <option value="fisica">Física</option>
-                                        <option value="juridica">Jurídica</option>
-                                    </select>
-                                </div>
+                    <form method="get" id="fj">
+                        <div class="field">
+                            <div class="control">
+                                <h2 class="title has-text-grey" aling="Center">Pessoa Física ou Jurídica:</h2>
+                                <select id="clientesfj" name="clientefj" class="control">
+                                    <option value="0"></option>
+                                    <option value="1" name="Fisica">Fisica</option>
+                                    <option value="2" name="Juridica">Juridica</option>
+                                </select>
                             </div>
-
+                        </div>
+                        <input type="submit" value="Pronto"/>
+                    </form>
+                    <div class="box" >
+                        <form action="cadastrar.php" method="Post">
                             <h2 class="title has-text-grey" aling="Center">Preencha os dados:</h2>
                             <div class="field">
                                 <div class="control">
                                     <input name="nome" type="text" class="input is-large" placeholder="Nome" autofocus>
                                 </div>
                             </div>
-                            
+                            <div class="field" id="cpf">
+                                <div class="control">
+                                    <input name="cpf" type="text" class="input is-large" placeholder="CPF" autofocus>
+                                </div>
+                            </div>
+                            <div class="field">
+                                <div class="control" id="cnpj">
+                                    <input name="cnpj" type="text" class="input is-large" placeholder="CNPJ" autofocus>
+                                </div>
+                            </div>
                             <div class="field">
                                 <div class="control">
                                     <input name="usuario" type="text" class="input is-large" placeholder="Usuário">
@@ -51,7 +79,7 @@
                             </div>
                             <div class="field">
                                 <div class="control">
-                                    <input name="senha" class="input is-large" type="password" placeholder="Senha">
+                                     <input name="senha" class="input is-large" type="password" placeholder="Senha">
                                 </div>
                             </div>
                             <button type="submit" class="button is-block is-link is-large is-fullwidth">Cadastrar</button>
@@ -62,5 +90,4 @@
         </div>
     </section>
 </body>
-
 </html>
